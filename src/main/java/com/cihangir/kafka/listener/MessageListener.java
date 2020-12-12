63d29edb4e3995eeb7dc908fc2ba10526c9ae790
Sefa Cihangir <sefa.cihangir@iottech.com.tr>
2020-12-12 14:52:28 +0300

    consume messages from kafka work implemented

--- src/main/java/com/cihangir/kafka/listener/MessageListener.java
+++ src/main/java/com/cihangir/kafka/listener/MessageListener.java
@@ -16,29 +16,20 @@ public class MessageListener {
 
     private final Logger log = LoggerFactory.getLogger(MessageListener.class);
 
-
-    /*
-    public void listenFirstTopicWithDetails(ConsumerRecord<String, MessageDto> consumerRecord,
-                                            @Payload MessageDto messageEntity,
-                                            @Header(KafkaHeaders.GROUP_ID) String groupId,
-                                            @Header(KafkaHeaders.OFFSET) int offset,
-                                            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition)
-    {
-        System.out.println("Received message with below details:");
-        System.out.println(consumerRecord); //We can also reach to all details from consumerRecord.
-        System.out.println(messageEntity); //This is value.
-        System.out.println(groupId); //This will be groupId1.
-        System.out.println(offset); //Current record offset.
-        System.out.println(partition); //We used only one partition, so this will be 0 for each message.
-    }*/
-
     @KafkaListener(
             topics = "${spring.kafka.template.second-topic}",
             groupId = "groupId1"
     )
-    public void listen(@Payload MessageDto message) {
+    public void listen(ConsumerRecord<String, MessageDto> consumerRecord,
+                       @Payload MessageDto message,
+                       @Header(KafkaHeaders.OFFSET) int offset,
+                       @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
         log.info("Message received.. MessageID : {} Message: {} Date : {}",
                 message.getMessage(),message.getCreatedDate());
+        System.out.println(consumerRecord); //We can also reach to all details from consumerRecord.
+        System.out.println(offset); //Current record offset.
+        System.out.println(partition); //We used only one partition, so this will be 0 for each message.
+
     }
 
 
